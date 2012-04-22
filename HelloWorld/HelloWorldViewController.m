6605fc72360aaf207d8140176f0b1d29cac13d32
Michael Sonnleitner <wrssmi@Michaels-MacBook-Pro.local>
2012-04-22 20:06:55 +0200

    greeting changed

--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -45,7 +45,7 @@
     {
         nameString = @"World";
     }
-    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!",nameString];
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@! You are great!",nameString];
     self.label.text = greeting;
 }
 
